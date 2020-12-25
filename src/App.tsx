6292c45a2c696f038ae17943d2fa82f58b8c258b
EmilyLip <mily7975@gmail.com>
2020-12-25 13:00:06 +0200

    added project dependecies

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from 'assets/images/Zoomit.png';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt='' />
+        <p> fdf</p>
+    </div>
+  );
+}
+
+export default App;
