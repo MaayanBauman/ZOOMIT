c4440f6e7a0e3c47fb52a2180dd042ecff604609
EmilyLip <mily7975@gmail.com>
2020-12-25 16:43:59 +0200

    added logo and greeting message

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,12 +1,21 @@
+import { Typography } from '@material-ui/core';
 import logo from 'assets/images/Zoomit.png';
 import useStyles from './LandingPageStyles';
 
 
 const LandingPage: React.FC = (): JSX.Element => {
     const classes = useStyles();
+    const aboutUsMessage = 'בZoomit מרכזים לך את כל אירועי הזום הכי חמים בתחומי עניין מגוונים.';
 
     return (
-          <div className={classes.backgroundIamge}></div>
+        <div className={classes.backgroundIamge + ' ' + classes.container}>
+            <div className={classes.logoContainer}>
+                <img src={logo} alt='logo'  className={classes.logo}/>
+            </div>
+            <Typography className={classes.aboutUsMessage}>
+                {aboutUsMessage}
+            </Typography>
+        </div>
     );
 }
 
--- src/components/LandingPage/LandingPageStyles.ts
+++ src/components/LandingPage/LandingPageStyles.ts
@@ -2,6 +2,16 @@ import { makeStyles } from '@material-ui/styles';
 import backgroundImage from 'assets/images/background.png';
 
 const useStyles = makeStyles(() => ({
+    container: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    logoContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        width: 'auto',
+        height: '30vh',
+    },
     backgroundIamge: {
         backgroundImage: `url(${backgroundImage})`,
         backgroundRepeat: 'no-repeat',
@@ -11,6 +21,15 @@ const useStyles = makeStyles(() => ({
         width: '100vw',
         height: '100vh',
         backgroundPosition: 'bottom'
+    }, 
+    logo: {
+        width: 'auto',
+        height: '50%',
+        marginTop: '9vh',
+    },
+    aboutUsMessage: {
+        display: 'flex',
+        justifyContent: 'center'
     }
 }));
 
