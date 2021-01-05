28bd8d830de234659556a6d2b187006acb754a4c
EmilyLip <mily7975@gmail.com>
2021-01-05 23:30:45 +0200

    added get request to get all the events

--- Client/React/src/components/Content/EventsPage/EventsPage.tsx
+++ Client/React/src/components/Content/EventsPage/EventsPage.tsx
@@ -1,8 +1,22 @@
+import React, {  useEffect } from 'react';
 import { Typography } from '@material-ui/core';
 
+import axios from 'utils/axios';
 
 const EventsPage: React.FC = (): JSX.Element => {
-   
+
+    const [event, setEvents] = React.useState<any>();
+
+    useEffect(()=>{
+        axios.get('/events')
+        .then((result : any)=> (
+            console.log(result)
+        ))
+        .catch((error: any)=> (
+            console.log(error)
+        ))
+    }, [])
+
     return (
         <div >
             <Typography>
