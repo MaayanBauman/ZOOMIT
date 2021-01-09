c756ca3df50e81f4105585ab7ff12ec3f50c4975
sapir.holzman <sapir.holzman@gmail.com>
2021-01-09 18:31:29 +0200

    add filter box

--- Client/React/src/components/Content/EventsPage/EventsPage.tsx
+++ Client/React/src/components/Content/EventsPage/EventsPage.tsx
@@ -1,5 +1,12 @@
 import React, {  useEffect } from 'react';
-import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
 import useEventPage from './useEventPage';
 import useStyles from './EventsPageStyles';
@@ -17,6 +24,25 @@ const EventsPage: React.FC = (): JSX.Element => {
     return (
         <>
             <div className={classes.container}>
+                <div className={classes.topbar}>
+                    <Typography className={classes.count} variant="subtitle1" gutterBottom>
+                        {events.length} זומים
+                    </Typography>
+                    <Paper component="form" className={classes.search}>
+                        <InputBase
+                            className={classes.input}
+                            placeholder="חפש זומים"
+                            inputProps={{ 'aria-label': 'search zoom events' }}
+                        />
+                        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                        <Divider className={classes.divider} orientation="vertical" />
+                        <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+                            <FilterListIcon />
+                        </IconButton>
+                    </Paper>
+                </div>
                 {
                     categories.map(category => (<EventCategoryRow key={category.id} events={events} title={category.name}/>) )
                 }
--- Client/React/src/components/Content/EventsPage/EventsPageStyles.ts
+++ Client/React/src/components/Content/EventsPage/EventsPageStyles.ts
@@ -1,13 +1,45 @@
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 
-const useStyles = makeStyles({
-   container: {
-       display:'flex',
-       flexDirection: 'column',
-       marginTop: '5vh',
-       width: '100vw'
-   }
-});
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            display:'flex',
+            flexDirection: 'column',
+            marginTop: '5vh',
+            fontFamily:'Assistant',
+            marginRight: '3vw'
+        },
+        topbar: {
+            direction: 'rtl',
+            display: 'flex',
+            flexDirection: 'row',
+        },
+        count:{
+            color: '#868692',
+            fontSize: 18,
+        },
+        search:{
+            padding: '2px 4px',
+            display: 'flex',
+            alignItems: 'center',
+            width: 400,
+            marginRight: '28vw',
+        },
+        input: {
+            marginLeft: theme.spacing(1),
+            flex: 1,
+            paddingRight: '2px',
+        },
+        iconButton: {
+            padding: 10,
+            color: '#0C63CE',
+        },
+        divider: {
+            height: 28,
+            margin: 4,
+        },
+    })
+);
 
 export default useStyles;
