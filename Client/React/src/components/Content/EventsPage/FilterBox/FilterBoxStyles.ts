4d6962df7f8b6814bf9aa22fe1a99c85c29b98b0
sapir.holzman <sapir.holzman@gmail.com>
2021-01-09 18:40:07 +0200

    export filter box to new file

---
+++ Client/React/src/components/Content/EventsPage/FilterBox/FilterBoxStyles.ts
@@ -0,0 +1,28 @@
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      search:{
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 400,
+        marginRight: '28vw',
+      },
+      input: {
+          marginLeft: theme.spacing(1),
+          flex: 1,
+          paddingRight: '2px',
+      },
+      iconButton: {
+          padding: 10,
+          color: '#0C63CE',
+      },
+      divider: {
+          height: 28,
+          margin: 4,
+      }
+    })
+);
+
+export default useStyles;
