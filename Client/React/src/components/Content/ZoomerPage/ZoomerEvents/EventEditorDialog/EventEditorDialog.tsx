50e1571afbc85487955be4e40146d8150a27dc6d
Emily Lipkin <emily7975@gmail.com>
2021-02-08 21:42:49 +0200

    feat(): create event (#20)
    Co-authored-by: EmilyLip <mily7975@gmail.com>

--- Client/React/src/components/Content/ZoomerPage/ZoomerEvents/EventEditorDialog/EventEditorDialog.tsx
+++ Client/React/src/components/Content/ZoomerPage/ZoomerEvents/EventEditorDialog/EventEditorDialog.tsx
@@ -1,16 +1,66 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import { CalendarToday, AccessTime } from '@material-ui/icons';
 import { DialogTitle,Button, Dialog, DialogActions ,Select ,MenuItem,
     DialogContent, TextField, InputLabel, InputAdornment  } from '@material-ui/core';
-import {  CalendarTodayOutlined } from '@material-ui/icons';
-
+import { KeyboardTimePicker, KeyboardDatePicker,} from '@material-ui/pickers';
+ 
 import Category from 'models/Category/Category';
+import StoreStateType from 'redux/storeStateType';
+
 import useStyles from './EventEditorDialogStyles';
+import useEventEditorDialog from './useEventEditorDialog';
 
 const EventEditorDialog : React.FC<Props> = (props : Props): JSX.Element => {
 
-    //const { categories } = ();
     const classes = useStyles();
     const {isOpen, handleClose, isEditMode} = props;
+    const categories = useSelector<StoreStateType,Category[]>(state=> state.categories);
+
+    const {startDate, setStartDate, endDate, setEndDate,handleDateChange,
+            eventName, setEventName, categoryId, setCategoryId, description,
+            setDescription, link, setLink, price, setPrice,
+            maxRegisters, setMaxRegisters, zoomPass, setZoomPass, createEvent } = useEventEditorDialog();
+
+    const datePickers = (
+        <>
+            <KeyboardDatePicker
+                className={classes.picker}
+                label="תאריך"
+                format="dd/MM/yyyy"
+                value={startDate}
+                onChange={(date)=> handleDateChange(date)}
+                showTodayButton
+                keyboardIcon={
+                    <CalendarToday className={classes.calendarIcon}/>
+                }
+            />
+            <KeyboardTimePicker
+                ampm={false}
+                className={classes.picker}
+                label="שעת התחלה"
+                value={startDate}
+                format='HH:mm'
+                onChange={(date)=> setStartDate(date)}
+                disableToolbar
+                keyboardIcon={
+                    <AccessTime className={classes.calendarIcon}/>
+                }
+            />
+             <KeyboardTimePicker
+                ampm={false}
+                className={classes.picker}
+                label="שעת סיום"
+                value={endDate}
+                format='HH:mm'
+                onChange={(date)=> setEndDate(date)}
+                disableToolbar
+                keyboardIcon={
+                    <AccessTime className={classes.calendarIcon}/>
+                }
+            />
+        </>
+    );
 
     return (
         <div>
@@ -22,35 +72,54 @@ const EventEditorDialog : React.FC<Props> = (props : Props): JSX.Element => {
                 <form >
                     <div className={classes.formRow}>
                         <InputLabel className={classes.label}>שם</InputLabel>
-                        <TextField className={classes.fieldInput} size="small" fullWidth/>
+                        <TextField size="small" fullWidth
+                            value={eventName}
+                            onChange={(event)=> setEventName(event.target.value)}/>
                     </div>
                     <div className={classes.formRow}>
                         <InputLabel className={classes.label}>קטגוריה</InputLabel>
                         <Select
-                         className={classes.fieldInput}
+                         value={categoryId}
                          fullWidth
+                         onChange={(event)=> setCategoryId(event.target.value)}
                         >
-                            <MenuItem dir='rtl' value={10}>אמילי</MenuItem>
+                            {categories.map((category: Category) => {
+                                return  <MenuItem dir='rtl' value={category.id}>{category.name}</MenuItem>
+                            })}
                         </Select>
                     </div>
-                    <div className={classes.formRow}>
-                        <InputLabel className={classes.label}>מועד</InputLabel>
-                        <TextField className={classes.fieldInput} size="small" fullWidth
-                         InputProps={{
-                            endAdornment: <CalendarTodayOutlined className={classes.calendarIcon}></CalendarTodayOutlined>
-                        }} />
+                    <div className={classes.datePickers + ' ' + classes.formRow}>
+                        {datePickers}
                     </div>
                     <div className={classes.formRow}>
                         <InputLabel className={classes.label}>תיאור</InputLabel>
-                        <TextField className={classes.fieldInput} size="small" fullWidth multiline/>
+                        <TextField size="small" fullWidth multiline
+                         value={description}
+                         onChange={(event)=> setDescription(event.target.value)}/>
+                    </div>
+                    <div className={classes.formRow}>
+                        <InputLabel >מס' משתתפים מקסימלי</InputLabel>
+                        <TextField size="small" fullWidth
+                         value={maxRegisters}
+                         onChange={(event)=> setMaxRegisters(event.target.value)}/>
                     </div>
                     <div className={classes.formRow}>
                         <InputLabel className={classes.label}>לינק</InputLabel>
-                        <TextField className={classes.fieldInput} size="small" fullWidth/>
+                        <TextField size="small" fullWidth
+                         value={link}
+                         onChange={(event)=> setLink(event.target.value)}/>
+                    </div>
+                    <div className={classes.formRow}>
+                        <InputLabel className={classes.label}>סיסמה</InputLabel>
+                        <TextField size="small" fullWidth
+                         value={zoomPass}
+                         onChange={(event)=> setZoomPass(event.target.value)}/>
                     </div>
                     <div className={classes.formRow}>
                         <InputLabel className={classes.label}>מחיר</InputLabel>
-                        <TextField className={classes.fieldInput} size="small" fullWidth
+                        <TextField size="small" fullWidth
+                            value={price}
+                            onChange={(event)=> setPrice(event.target.value)}
                             InputProps={{
                                 endAdornment: <InputAdornment position="end">₪</InputAdornment>
                             }} 
@@ -59,10 +128,15 @@ const EventEditorDialog : React.FC<Props> = (props : Props): JSX.Element => {
                 </form>
                 </DialogContent>
                 <DialogActions>
-                    <Button color="primary" variant="contained">
+                    <Button color="primary" variant="contained" className={classes.dialogButton} 
+                        onClick={()=> {
+                            createEvent();
+                            handleClose();
+                        }}>
                        אישור
                     </Button>
-                    <Button color="secondary" variant="contained">
+                    <Button color="secondary" variant="contained" className={classes.dialogButton} 
+                        onClick={handleClose}>
                        ביטול
                     </Button>
                 </DialogActions>
---
+++ Client/React/src/components/Content/ZoomerPage/ZoomerEvents/EventEditorDialog/useEventEditorDialog.ts
@@ -0,0 +1,79 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+import axios from 'utils/axios';
+import User from 'models/User/User'; 
+import Event from 'models/Event/Event';
+import storeStateType from 'redux/storeStateType' ; 
+import {useZoomerPageOutCome} from './useEventEditorDialogInterfaces';
+import StoreStateType from 'redux/storeStateType';
+
+const useEventEditorDialog  = () : useZoomerPageOutCome  => {
+
+    const zoomer = useSelector<StoreStateType, User>(state=> state.user);
+    const [startDate, setStartDate] = React.useState<Date | null>(new Date());
+    const [endDate, setEndDate] = React.useState<Date | null>(new Date());
+    const [eventName, setEventName] = React.useState<string>('');
+    const [categoryId, setCategoryId] = React.useState<string>();
+    const [description, setDescription] = React.useState<string>('');
+    const [link, setLink] = React.useState<string>('');
+    const [price, setPrice] = React.useState<number>(0);
+    const [maxRegisters, setMaxRegisters] = React.useState<number>(100);
+    const [zoomPass, setZoomPass] = React.useState<string>('');
+
+
+    const handleDateChange = (date: Date | null) => {
+        setStartDate(date);
+        setEndDate(date);
+    };
+    
+    const createEvent = () => {
+
+        const newEvent : Event = {
+            id: '',
+            title: eventName,
+            description: description,
+            zoomer_id: zoomer._id,
+            zoom_link: link,
+            password: zoomPass,
+            start_time: startDate as Date,
+            end_time: endDate as Date,
+            max_registers: maxRegisters,
+            registered_users: [],
+            category: categoryId as string,
+            price: price,
+            source_id: ''
+        }
+
+        axios.post('/events', {event: newEvent}).then((res)=> {
+            console.log(res);
+        }).catch((err)=> {
+            console.log(err);
+        })
+    }
+
+    return {
+        startDate,
+        setStartDate,
+        endDate,
+        setEndDate,
+        handleDateChange,
+        eventName,
+        setEventName,
+        categoryId,
+        setCategoryId,
+        description,
+        setDescription,
+        link,
+        setLink,
+        price,
+        setPrice,
+        maxRegisters,
+        setMaxRegisters,
+        zoomPass,
+        setZoomPass,
+        createEvent
+    }
+}
+
+export default useEventEditorDialog;
