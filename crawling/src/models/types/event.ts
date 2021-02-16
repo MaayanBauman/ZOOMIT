cf60128d001d30a2653f06217cc69912c471fdcd
EmilyLip <mily7975@gmail.com>
2021-02-17 00:24:40 +0200

    scraping detailes

--- crawling/src/models/types/event.ts
+++ crawling/src/models/types/event.ts
@@ -1,17 +1,17 @@
 import { Decimal128, ObjectId } from "mongodb";
 
 export interface IEvent {
-    _id: ObjectId;
+    //_id: ObjectId;
     title: string;
     description: string;
-    zoomer_id: ObjectId;
+    //zoomer_id: ObjectId;
     zoom_link: string;
     password: string
     start_time: Date;
     end_time: string;
     max_registers: number;
-    registered_users: Array<string>
-    category: ObjectId;
-    price: Decimal128;
-    source_id: ObjectId;
+    //registered_users: Array<string>
+    category: string;
+    price: number;
+    source_id: string;
 }
--- crawling/src/services/scraper.ts
+++ crawling/src/services/scraper.ts
@@ -4,25 +4,52 @@ import cheerio from 'cheerio';
 import {IEvent}  from '../models/types/event';
 
 const scrape = async () => {
-    let event: IEvent;
+    let events: IEvent[]| undefined = [];
 
     const page = await axios.get('https://screenz.live/categories/psychology')    
     const $ = cheerio.load(page.data);    
     const zooms = $('div[class^="eventsGrid"]');
     zooms.each(() => {
-        // $('div[class^="event"]', this).each((num, elemnt) => {
             $('p[class*="live"]', this).each((num, e: cheerio.TagElement) => {
+                let newEvent : IEvent = {
+                    title: '',
+                    description:  '',
+                    zoom_link: '',
+                    password: '',
+                    start_time: null,
+                    end_time: null,
+                    max_registers: 100,
+                    category: '602c1aef6908bb23ebfc8766',
+                    price: 0,
+                    source_id: '602c1b5048cf724be45cc78e'
+                };
                let sybling: cheerio.TagElement = e.nextSibling as cheerio.TagElement;
                console.log(sybling.childNodes[0].data);
+               newEvent.title = sybling.childNodes[0].data;
+
                sybling = sybling.nextSibling as cheerio.TagElement;
                let childNode : cheerio.TagElement[] = sybling.childNodes as cheerio.TagElement[];
-               childNode = childNode.
-               childNode.map(child =>{
-                console.log(child.nodeValue);
+               childNode = childNode[0].childNodes as cheerio.TagElement[];
+               childNode.map((child: cheerio.TagElement, index) =>{
+                   const dates: cheerio.TagElement[] =  child.childNodes as cheerio.TagElement[];
+                   if(index == 0){
+                       console.log('time:' + dates[0]?.nodeValue);
+                       var parts =dates[0]?.nodeValue.split(':');
+                       newEvent.start_time = new Date(2000, 1, 1, +parts[0],+parts[1]);
+                   }
+                   else if(index == 1) {
+                       console.log('date:' + dates[0]?.nodeValue)
+                       var parts =dates[0]?.nodeValue.split('.');
+                       var hour = newEvent.start_time.getHours();
+                       var minute =  newEvent.start_time.getMinutes();
+                       newEvent.start_time = new Date(2000 + (+parts[2]), +parts[1] - 1, +parts[0], hour, minute);
+                   }
                })
+               events.push(newEvent);
             });
-            })
-        // });     
+            })     
+
+            console.log(events);
 };
 
 export default scrape;
