82d0b624c84f1cb428a5970899994906ab0309b7
sapir.holzman <sapir.holzman@gmail.com>
2021-02-17 19:47:39 +0200

    use server API getById

--- Client/React/src/components/Content/EventsPage/EventCard/useEventCard.ts
+++ Client/React/src/components/Content/EventsPage/EventCard/useEventCard.ts
@@ -1,46 +1,26 @@
-import { useEffect, useState } from 'react';
 import axios from 'utils/axios';
-import User from 'models/User/User';
 import { useSelector } from 'react-redux';
 import Category from 'models/Category/Category';
 import StoreStateType from 'redux/storeStateType';
-import Source from 'models/Source/Source';
 
 const useEventCard = () : useEventCardOutCome  => {
-    const [users, setUsers] = useState<User[]|[]>([]);
-    const [sources, setSources] = useState<Source[]|[]>([]);
     const categories = useSelector<StoreStateType, Category[]>(state=> state.categories);
 
-    const getUsers = () => {
-        axios.get('/users').then((result : any) => {
-            setUsers(result.data);
-         }).catch((error: any)=> (
-            console.log(error)
-        ));
-    }
-    const getSources = () => {
-        axios.get('/sources').then((result : any) => {
-            setSources(result.data);
-         }).catch((error: any)=> (
-            console.log(error)
-        ));
-    }
-
     const getSourceById = (sourceId: string) => {
-        const source = sources.find(source => source._id === sourceId )
-        return source;
+        axios.get(`/sources/${sourceId}`).then((result : any) => {
+            return(result.data);
+         }).catch((error: any) => (
+            console.log(error)
+        ));
     }
     
     const getUserById = (userId: string) => {
-        const user = users.find(user => user._id === userId )
-        return user;
+        axios.get(`/users/${userId}`).then((result : any) => {
+            return(result.data);
+         }).catch((error: any) => (
+            console.log(error)
+        ));
     }
-    
-    useEffect(() => {
-        getUsers();
-        getSources();
-    }, []);
-
     return {
         getUserById,
         categories,
