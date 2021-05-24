aa550c506fb0290febb4d2346f494d621b9d4722
sapir.holzman <sapir.holzman@gmail.com>
2021-05-24 22:26:59 +0300

    add providers to angular service

--- Client/Angular/src/app/app-routing.module.ts
+++ Client/Angular/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PathLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: LocationStrategy, useClass: PathLocationStrategy}]
 })
 
 export class AppRoutingModule { }
