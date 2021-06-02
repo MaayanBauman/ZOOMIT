6b794607b09c6ec8bc626b70bfdb441aae293d6c
Zohar-UZ <Zohar96@gmail.com>
2021-06-02 19:39:32 +0300

    recommendation algo (#38)

--- Server/src/services/users.ts
+++ Server/src/services/users.ts
@@ -1,24 +1,26 @@
+//@ts-nocheck
 import { Request, Response } from 'express';
 import { IOperationBuilder } from './operations/types'
 import operationBuilder from './operations';
 import { IUser } from '../models/types/user';
-import { 
-    addUserToEvent, 
-    getEventsById, 
-    removeUserFromEvent, 
+import {
+    addUserToEvent,
+    getEventsById,
+    removeUserFromEvent,
     getEventsByIdJoinedAndSorted,
-    getEventById 
+    getEventById
 } from './events';
 import config from '../config';
 import sendEmail from '../repositories/sendEmail';
+import getRecommendedEventsIds from '../RecommenderAlgo/algo';
 
 const collectionName = config.collections.users.name;
 const usersOperationBuilder: IOperationBuilder<IUser> = operationBuilder<IUser>();
 
-export const getAllUsers = () => 
+export const getAllUsers = () =>
     usersOperationBuilder.getAllObjects(collectionName);
 
-export const getUserById = (id: string) => 
+export const getUserById = (id: string) =>
     usersOperationBuilder.getObjectById(collectionName, id);
 
 export const getUsersByType = (type: string) =>
@@ -50,12 +52,24 @@ export const getZoomerRequesters = () => {
     return usersOperationBuilder.getObjectsBySubsetFiled(collectionName, 'is_waiting_for_approval', [true]);
 }
 
-export const addUser = (newSource: IUser) => 
+export const getUserRecommendedEvents = async (id: string, count: Number) => {
+    const userRecommendedEventsIds = await getRecommendedEventsIds(id, count);
+    const userRecommendedEvents = await getEventsById(userRecommendedEventsIds.map(e => e._id));
+    return userRecommendedEvents
+        .map(event => {
+            // Add user's rating to event
+            const index = userRecommendedEventsIds.findIndex(e => e["_id"] == event._id);
+            // const rating = user.registerd_events[index].rating;
+            return { ...event, score: userRecommendedEventsIds[index].score };
+        }).sort((a, b) => { return b.score - a.score });
+}
+
+export const addUser = (newSource: IUser) =>
     usersOperationBuilder.createObject(collectionName, newSource);
 
-export const addEventToUser = (id: string, eventid: string) => 
+export const addEventToUser = (id: string, eventid: string) =>
     Promise.all([
-        addUserToEvent(eventid, id), 
+        addUserToEvent(eventid, id),
         usersOperationBuilder.addUniqueValuesToArray(collectionName, id, 'registerd_events', [{ eventId: eventid, rating: 0 }])
     ]).then(async () => {
         const user = await getUserById(id);
@@ -63,30 +77,29 @@ export const addEventToUser = (id: string, eventid: string) =>
         sendEmail(user, fullEvent);
     });
 
-    //TODO!!!
-export const removeEventFromUser = (id: string, eventId: string) => 
+//TODO!!!
+export const removeEventFromUser = (id: string, eventId: string) =>
     Promise.all([
-        removeUserFromEvent(eventId, id), 
+        removeUserFromEvent(eventId, id),
         usersOperationBuilder.deleteValueFromArray(collectionName, id, 'registerd_events', { eventId })
     ]);
 
-export const addEventToZoomer = (id:string, event: string) => 
-    Promise.all([ 
+export const addEventToZoomer = (id: string, event: string) =>
+    Promise.all([
         usersOperationBuilder.addUniqueValuesToArray(collectionName, id, 'owned_events', [event])
     ]);
 
-export const deleteEventFromZoomer = (id:string, event: string) => 
+export const deleteEventFromZoomer = (id: string, event: string) =>
     Promise.all([
-        removeUserFromEvent(event, id), 
+        removeUserFromEvent(event, id),
         usersOperationBuilder.deleteValuesFromArray(collectionName, id, 'owned_events', [event])
     ]);
 
-export const updateUser = (id: string, sourceToUpdate: IUser) => 
+export const updateUser = (id: string, sourceToUpdate: IUser) =>
     usersOperationBuilder.updateObject(collectionName, id, sourceToUpdate);
 
-export const updateEventFromUser = (id: string, eventIndex: number, eventId: string, rating: number) => 
+export const updateEventFromUser = (id: string, eventIndex: number, eventId: string, rating: number) =>
     usersOperationBuilder.updateValueOnArrayByIndex(collectionName, id, `registerd_events.${eventIndex}`, { eventId, rating })
 
-export const deleteUser = (id: string) => 
+export const deleteUser = (id: string) =>
     usersOperationBuilder.deleteObject(collectionName, id);
-
